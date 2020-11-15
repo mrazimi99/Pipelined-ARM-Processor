e7df478ed9aaa15ee4f938bebcb564f4004b8c51
Ali Jafarzadeh <a.jafarzadeh1998@gmail.com>
2020-11-15 16:04:47 +0330

    just add condition check file and Controler file!

---
+++ IDSTAGE/ConditionCheck.v
@@ -0,0 +1,3 @@
+module ConditionCheck (input[3: 0] cond, input [31 :0] status,output result);
+
+endmodule // ConditionCheck
---
+++ IDSTAGE/Controler.v
@@ -0,0 +1,4 @@
+module ControlUnit (input[1: 0] mode, input[3: 0] op_code, input s, output aluCommand,
+                    output mem_read, mem_write, wb_en, is_imm, branch, status_en);
+
+endmodule // ControlUnit
