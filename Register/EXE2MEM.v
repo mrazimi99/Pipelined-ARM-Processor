18d0284ef27152ecf51f525bfdd29ff3888cb404
Ali Jafarzadeh <a.jafarzadeh1998@gmail.com>
2020-12-03 02:36:53 +0330

    add Exe2mem Register

--- Register/EXE2MEM.v
+++ Register/EXE2MEM.v
@@ -1,12 +1,25 @@
-module EXE2MEM (input clk, rst, input[31: 0] pc_in, output reg[31: 0] pc);
+module EXE2MEM (input clk, rst, wb_en_in, mem_read_in, mem_write_in,
+                input[3: 0] dest_in, input[31: 0] result_in, reg2_in,
+                output reg wb_en_out, mem_read_out, mem_write_out,
+                output reg[3: 0] dest_out, output reg[31: 0] result_out, reg2_out);
 
   always @ (posedge clk, posedge rst) begin
 
     if (rst) begin
-      pc <= 32'b 0;
+      wb_en_out <= 0;
+      mem_read_out <= 0;
+      mem_write_out <= 0;
+      dest_out <= 0;
+      result_out <= 0;
+      reg2_out <= 0;
     end
     else begin
-      pc <= pc_in;
+      wb_en_out <= wb_en_in;
+      mem_read_out <= mem_read_in;
+      mem_write_out <= mem_write_in;
+      dest_out <= dest_in;
+      result_out <= result_in;
+      reg2_out <= reg2_in;
     end
 
   end
