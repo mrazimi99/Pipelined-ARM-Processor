6108becc1255aa56d06c2ee60c770e002e4b417a
mr azimi <mrazimi99@gmail.com>
2020-12-03 19:45:49 +0330

    Minor changes.

--- IDSTAGE/IDtb.v
+++ IDSTAGE/IDtb.v
@@ -9,10 +9,10 @@ module IDtb ();
   wire status_en, mem_read, mem_write, wb_en, branch, I;
 
   IFSTAGE ifStage(.clk(clk), .rst(rst), .freeze(1'b 0), .branch_track(1'b 0),
-                  .branch_addr(32'b 0), .instruction(instruction), .pc(pc_if_out));
+                  .branch_addr(32'b 0), .instruction(instruction), .pc(pc_id_in));
 
   IDSTAGE idStage(.clk(clk), .rst(rst), .write_back_en(1'b 0), .hazard(1'b 0), .pc_in(pc_id_in),
-                  .instruction(instruction), .reg_data_wb(32'b 0), .dest_wb(4'b 0), status(status),
+                  .instruction(instruction), .reg_data_wb(32'b 0), .dest_wb(4'b 0), .status(status),
                   .pc(pc_id_out), .reg1(reg1), .reg2(reg2), .aluCommand(aluCommand),
                   .dest(dest), .status_en(status_en), .mem_read(mem_read),
                   .mem_write(mem_write), .wb_en(wb_en), .branch(branch), .I(I),
--- IFSTAGE/instructionSet.v
+++ IFSTAGE/instructionSet.v
@@ -1,5 +1,5 @@
 module InstrcutionSet (input[31 :0] address ,output [31 :0] instructionOut);
   reg [7 :0] instruction[0 : 4095];
-  initial begin $readmemb("IFSTAGE\\instructions.txt", instruction); end
+  initial begin $readmemb("instructions.txt", instruction); end
   assign instructionOut = {instruction[address] ,instruction[address + 1] ,instruction[address + 2] ,instruction[address + 3]};
 endmodule // InstrcutionSet
