ff6ddd2a6f800a974800ef5ac060bf73cabb2929
Ali Jafarzadeh <a.jafarzadeh1998@gmail.com>
2020-12-10 11:51:47 +0330

    add Memory Stage

--- MEMSTAGE/MEMstage.v
+++ MEMSTAGE/MEMstage.v
@@ -1,5 +1,16 @@
-module MEMstage (input clk, rst, input[31: 0] pc_in, output[31: 0] pc);
+module MEMstage (input clk, rst, write_en, read_en, input[31: 0] input_data,
+                 address, output[31: 0] data);
 
-  assign pc = pc_in;
+  reg [31:0] registers [0: 64*1024]; //2^16 = 64KB
+
+  assign data = read_en ? registers[address] : 0;
+
+  integer i;
+  always @ (posedge clk, posedge rst) begin
+    if (rst)
+      for(i = 0 ; i < 64*1024 ; i = i+1) registers[i] = 0;
+    if (write_en)
+      registers[address] <= input_data;
+  end
 
 endmodule // MEMstage
--- Register/MEM2WB.v
+++ Register/MEM2WB.v
@@ -1,12 +1,23 @@
-module MEM2WB (input clk, rst, input[31: 0] pc_in, output reg[31: 0] pc);
+module MEM2WB (input clk, rst, wb_en_in, mem_read_in, input[3: 0] dest_in,
+               input[31: 0] alu_result_in, mem_data_in, output reg wb_en_out,
+               mem_read_out, output reg[3: 0] dest_out, output reg[31: 0] mem_data_out,
+               alu_result_out);
 
   always @ (posedge clk, posedge rst) begin
 
     if (rst) begin
-      pc <= 32'b 0;
+      wb_en_out <= 0;
+      mem_read_out <= 0;
+      dest_out <= 0;
+      mem_data_out <= 0;
+      alu_result_out <= 0;
     end
     else begin
-      pc <= pc_in;
+      wb_en_out <= wb_en_in;
+      mem_read_out <= mem_read_in;
+      dest_out <= dest_in;
+      mem_data_out <= mem_data_in;
+      alu_result_out <= alu_result_in;
     end
 
   end
