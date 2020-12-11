40447a7c4b4ee168e9c31a82f845552287c3ee5d
jafarzadeh <a.jafarzadeh1998@gmail.com>
2020-12-11 14:49:32 +0330

    fix the simulation's bugs

--- signExtend.v
+++ signExtend.v
@@ -1,4 +1,6 @@
-module SignExtend #(parameter N)(input [12:0] in,output [31:0] out);
+module SignExtend #(parameter N)( in, out);
+  input [N-1 :0] in;
+  output [31:0] out;
   // assign out = (in[N-1] == 1) ? {1'b 1 >>> 31-N, in} : {1'b 0  >>> 31-N , in};
   assign out = in >>> (32 - N);
 endmodule // signExtend
