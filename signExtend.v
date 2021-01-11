752089b3d96f029516d0605f3108168a5ea7877b
mr azimi <mrazimi99@gmail.com>
2021-01-12 00:33:46 +0330

    All bugs before forwarding fixed.

--- signExtend.v
+++ signExtend.v
@@ -1,6 +1,5 @@
 module SignExtend #(parameter N)( in, out);
   input [N-1 :0] in;
   output [31:0] out;
-  // assign out = (in[N-1] == 1) ? {1'b 1 >>> 31-N, in} : {1'b 0  >>> 31-N , in};
-  assign out = in >>> (32 - N);
+  assign out = {{(31 - N){in[N-1]}}, in};
 endmodule // signExtend
